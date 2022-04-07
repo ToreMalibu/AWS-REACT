2e471b6bf52c7d731642f2a75d2a88b3102fcfc9
ToreMalibu <toresescape@gmail.com>
2022-04-06 17:04:03 -0700

    adding backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "crudapp02": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
